3dac59c4b946dcbdf1021229f1c1912e7f4c9d2a
Hisham Mohamed <heshamkoptan@gmail.com>
2026-03-11 09:24:43 +0200

    deploy

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd domains/prod.live90.fr/public_html
-cp .prod.env .env
-php artisan optimize:clear
-php artisan config:cache
-php artisan migrate --force
-echo "✅ Deployment Successful!"
